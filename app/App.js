938d78b1d072a6d28f45defada4618c3a206c26e
Yanshen <yanshenlin1001@gmail.com>
2021-11-10 12:18:52 -0500

    Sign-in sign up done

--- app/App.js
+++ app/App.js
@@ -4,6 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import Home from './pages/Home/Home';
 import { NativeRouter, Switch, Route } from 'react-router-native';
 import Menu from './pages/Menu/Menu';
+import SignUp from './pages/Auth/SignUp'
+import SignIn from './pages/Auth/SignIn'
 
 export default function App() {
   return (
@@ -11,8 +13,10 @@ export default function App() {
       <View style={styles.container}>
 
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/menu" component={Menu}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/menu" component={Menu} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signin" component={SignIn} />
         </Switch>
       </View>
     </NativeRouter>
@@ -23,7 +27,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
--- app/pages/Home/Home.js
+++ app/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Text, View, Button} from 'react-native';
+import { Text, View, Button } from 'react-native';
 import styles from './Home.style'
 
-const Home = ({history}) => {
+const Home = ({ history }) => {
   return (
-    <View style={ styles.container }>
+    <View style={styles.container}>
       <Text>Home Page</Text>
-      <Button title="Go to menu page"  onPress={() => history.push('/menu')}/>
+      <Button title="Go to menu page" onPress={() => history.push('/menu')} />
+      <Button title="Go to Authentication page" onPress={() => history.push('/signin')} />
     </View>
   );
 };
