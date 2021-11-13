d28690e623af95c02fd2291fed82ab83f7246f64
Yanshen <yanshenlin1001@gmail.com>
2021-11-13 12:32:09 -0500

    Finish profile page

--- app/pages/Profile/Profile.js
+++ app/pages/Profile/Profile.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Text, View, Button, Linking, Dimensions, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, ScrollView, Button, Linking, Dimensions, TouchableOpacity, TextInput, Image } from 'react-native';
 import styles from './Profile.style'
 
 
@@ -8,53 +9,106 @@ import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 
 // To do: Add the function to recceive the input value and authentication on the value input
 
-
 const Profile = ({ history }) => {
     return (
-        <View style={styles.container}>
+        <View style={styles.container} blurRadius={1}>
 
             {/* Name  ->   Current Order + waiting time  ->  Order history(in one weeks) */}
             {/* At the top => will be able to view the overall menus  (can be direct to the menus page.) */}
 
-            <View style={styles.menuContainer}>
-                <TouchableOpacity onPress={() => history.push('menu')}>
-                    <Text>View Menus</Text>
+
+            <View style={styles.headerSection}>
+                <Text style={styles.headerText}>Welcome, Joe Doe</Text>
+            </View>
+
+            <View>
+            </View>
+
+
+            <View style={styles.currentOrder}>
+                <Text style={styles.currentOrderText}>
+                    Current Order Waiting Time
+                </Text>
+            </View>
+
+            <View style={styles.TimeContainer}>
+                <StatusBar style='light-content' />
+                <TouchableOpacity onPress={() => null} style={styles.button}>
+                    {/* The time will become dynamic when the database is ready */}
+                    <Text style={styles.buttonText}>60 mins</Text>
                 </TouchableOpacity>
             </View>
-            <View>
-                <Text>First name, Last name</Text>
+
+
+
+
+
+            <View style={styles.subTitle}>
+                <Text style={styles.subTitleText}>15 Large Cheese Pizza, 2 sodas, 4 bags of buffalo chicken wings</Text>
             </View>
 
 
-            <View>
-                <Text>
-                    Current Order + Waiting Time
+
+
+            <View style={styles.currentOrder}>
+                <Text style={styles.currentOrderText}>
+                    Order History
                 </Text>
             </View>
 
+            {/* This will do the scrollview list of the order */}
 
 
-
-            <View>
-                <Text>
-                    Order History section
+            <ScrollView style={styles.orderHistory}>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
                 </Text>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+                <Text style={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </Text>
+            </ScrollView>
+
+
+
+
+
+
+
+            <View style={styles.menucontainer}>
+                <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/menu')}>
+                    <Text style={styles.textcreate}>Browse Menus</Text>
+                </TouchableOpacity>
             </View>
-
-
-
-
-            <Text> This is profile page</Text>
-
-
-            <View style={styles.container3}>
-                <Text>Hello</Text>
-            </View>
-
-
-            <TouchableOpacity onPress={() => history.push('/signin')}>
-                <Text>Create Account?</Text>
-            </TouchableOpacity>
 
             <View style={styles.container3}></View>
 
--- app/pages/Profile/Profile.style.js
+++ app/pages/Profile/Profile.style.js
@@ -1,16 +1,121 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const screen = Dimensions.get('window')
+
 
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'lightblue',
-        padding: 120,
+        backgroundColor: '#fff',
+        padding: 15,
     },
     container3: {
         flex: 2,
         justifyContent: 'center',
         alignItems: 'center'
     },
+
+    headerSection: {
+        flex: 3,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 18,
+        borderBottomWidth: 3,
+        borderStyle: 'dashed',
+
+    },
+
+    headerText: {
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 32
+    },
+
+    currentOrder: {
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        paddingLeft: 2,
+        padding: 2,
+    },
+
+    TimeContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 20,
+        padding: 10
+    },
+
+    currentOrderText: {
+        color: 'red',
+        fontStyle: 'italic',
+        fontSize: 25,
+    },
+
+
+    button: {
+        borderWidth: 5,
+        borderColor: '#B9AAFF',
+        width: screen.width / 3.5,
+        height: screen.height / 7,
+        borderRadius: screen.width / 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        fontSize: 24,
+        color: '#B9AAFF',
+    },
+
+
+
+    subTitle: {
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    subTitle2: {
+        padding: 4,
+        marginLeft: 25,
+    },
+
+    subTitleText: {
+        fontSize: 19,
+    },
+
+    subItems: {
+        width: '75%',
+    },
+
+
+
+    menucontainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 30,
+    },
+
+    createAccount: {
+        width: '50%',
+        height: 60,
+        color: "white",
+        backgroundColor: "black",
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 15
+    },
+
+    textcreate: {
+        fontSize: 20,
+        color: "white",
+        fontWeight: "bold"
+    },
+
+    orderHistory: {
+        height: screen.height / 5,
+    }
 
 
 });
