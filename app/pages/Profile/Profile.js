ffa17ba7c41f63c69d415280e42b06793a170020
Yanshen <yanshenlin1001@gmail.com>
2021-11-13 13:28:44 -0500

    done

--- app/pages/Profile/Profile.js
+++ app/pages/Profile/Profile.js
@@ -87,15 +87,6 @@ const Profile = ({ history }) => {
                     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                     culpa qui officia deserunt mollit anim id est laborum.
                 </Text>
-                <Text style={styles.text}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                </Text>
             </ScrollView>
 
 
