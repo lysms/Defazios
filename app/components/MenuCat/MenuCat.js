bb0845f0c2d25c087854da620fb55ff156259c91
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-11-20 17:06:34 -0500

    dynamically getting catering menu

--- app/components/MenuCat/MenuCat.js
+++ app/components/MenuCat/MenuCat.js
@@ -3,70 +3,14 @@ import { Button, View, Text, TouchableOpacity } from 'react-native';
 import styles from './MenuCat.style';
 
 const MenuCat = props => {
-  const cats = [
-    {
-      "title": "Popular",
-      "catNum": "1"
-    }, 
-    {
-      "title": "Limited Features",
-      "catNum": "2"
-    },
-    {
-      "title": "Antipasto",
-      "catNum": "3"
-    },
-    {
-      "title": "Pizzas",
-      "catNum": "4"
-    },
-    {
-      "title": "Calzones",
-      "catNum": "5"
-    },
-    {
-      "title": "Pasta",
-      "catNum": "6"
-    },
-    {
-      "title": "Stromboli",
-      "catNum": "7"
-    },
-    {
-      "title": "Side Orders",
-      "catNum": "8"
-    },
-    {
-      "title": "Pizza Kit",
-      "catNum": "9"
-    },
-    {
-      "title": "Breakfast/ Lunch Features",
-      "catNum": "10"
-    },
-    {
-      "title": "Beverages",
-      "catNum": "11"
-    },
-    {
-      "title": "Desserts",
-      "catNum": "12"
-    },
-    {
-      "title": "Fall Features",
-      "catNum": "13"
-    }
-  ];
-  
   return (
     <View style={{ marginTop: 20 }}>
-      
       { 
-        cats.map(el => {
+        props.menu.map(el => {
             return (
-              <TouchableOpacity key={ el.catNum } onPress={props.handler}>
+              <TouchableOpacity key={ el.id } onPress={props.handler}>
                 <View style={styles.menuCat}>
-                  <Text style={styles.catName}>{el.title}</Text>
+                  <Text style={styles.catName}>{el.category}</Text>
                 </View>
               </TouchableOpacity>
             )
--- app/pages/Menu/Menu.js
+++ app/pages/Menu/Menu.js
@@ -1,42 +1,81 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {SafeAreaView, View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import { useLocation } from 'react-router-native'
+
+
 import AppButton from '../../components/Button/Button.component';
 // import COLORS from '../constants/colors';
-
 import MenuItem from '../../components/MenuItem/MenuItem';
 import MenuCat from '../../components/MenuCat/MenuCat';
 import MenuItemDetail from '../../components/MenuItemDetail/MenuItemDetail';
 
 import styles from './Menu.style';
 
+import firebase from '../../firebase';
+
 const Menu = ({history}) => {
 
+  // allows us to add props to a link
+  const location = useLocation();
+  let type = location.state?.type;
+  if (!type) {
+    type = "menu";
+  }
+
+  // state
   const [step, setStep] = useState('categories');
+  const [currentMenuType, setCurrentMenuType] = useState('cateringMenu');
+  const [currentMenu, setCurrentMenu] = useState([])
+
+  useEffect(() => {
+    // gets data from database
+    const catmen = firebase.firestore().collection('cateringMenu');
+    
+    if (currentMenu.length == 0) {
+
+      catmen.onSnapshot((querySnapShot) => {
+  
+        let tempData = []
+  
+        querySnapShot.forEach(res => {
+          const d = res.data();
+          let newitem = {
+            name: d.name, 
+            hcost: d.halfCost,
+            fcost: d.fullCost,
+            category: d.category, 
+            id: res.id
+          }
+          tempData.push(newitem)
+        })
+  
+        setCurrentMenu(prevData => [...prevData, ...tempData])
+      });
+    }
+  }, [currentMenu])
 
   
   const filterItemsHandler = () => {
     setStep('items');
   }
-
   const itemDetailHandler = () => {
     setStep('itemDetail');
   }
-
   const catHandler = () => {
     setStep('categories');
   }
 
   
-  let menuType = <MenuCat />
+  let menuStep = <MenuCat />
 
   if (step == "categories") {
-    menuType = <MenuCat handler={filterItemsHandler}/>
+    menuStep = <MenuCat handler={filterItemsHandler} menu={currentMenu}/>
   }
   else if (step == "items") {
-    menuType = <MenuItem handler={itemDetailHandler} back={catHandler}/>
+    menuStep = <MenuItem handler={itemDetailHandler} back={catHandler}/>
   }
   else if (step == "itemDetail") {
-    menuType = <MenuItemDetail back={filterItemsHandler}/>
+    menuStep = <MenuItemDetail back={filterItemsHandler}/>
   }
 
   return (
@@ -45,7 +84,7 @@ const Menu = ({history}) => {
         <AppButton h={history}/>
       </View>
       <ScrollView style={styles.scrollContainer}>
-        { menuType }
+        { menuStep }
       </ScrollView>
     </SafeAreaView>
 
