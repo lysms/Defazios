982f18ee869da4b72ad89cb87e2f5a448c4d6167
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-12-01 10:23:47 -0500

    creating base form for payment

--- app/pages/Payment/Payment.js
+++ app/pages/Payment/Payment.js
@@ -1,23 +1,58 @@
 import React from 'react'
-import { Text, View, Button, Linking, Dimensions, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View, TextInput, Button } from 'react-native';
 import styles from './Payment.style'
+import { Formik } from 'formik';
 
 import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 import HomeButton from '../../components/HomeButton/HomeButton';
-const Payment = ({ history }) => {
-    return (
-        <View style={styles.container}>
-            <HomeButton h={history} />
-            {/* <TouchableOpacity onPress={() => history.push('/ordering')}>
-                <View >
-                    <MaterialIcons name="home" size={32} color="red" />
-                </View>
-            </TouchableOpacity> */}
 
-            <Text>
-                Welcome to the payment section
-            </Text>
-        </View>
-    );
+const Payment = ({ history }) => {
+  return (
+    <View style={styles.container}>
+      <HomeButton h={history} />
+  
+        <Formik
+          initialValues={{
+            email: '',
+            fname: '',
+            lname: '',
+            specialRequests: ''
+          }}
+          onSubmit={values => console.log(values)}
+        >
+          {({handleChange, handleBlur, handleSubmit, values}) => (
+            <View>
+              <TextInput 
+                placeholder="Email"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+              <TextInput 
+                placeholder="First Name"
+                onChangeText={handleChange('fname')}
+                onBlur={handleBlur('fname')}
+                value={values.fname}
+              />
+              <TextInput 
+                placeholder="Last Name"
+                onChangeText={handleChange('lname')}
+                onBlur={handleBlur('lname')}
+                value={values.lname}
+              />
+              <TextInput 
+                multiline={true}
+                placeholder="Special Requests"
+                onChangeText={handleChange('specialRequests')}
+                onBlur={handleBlur('specialRequests')}
+                value={values.specialRequests}
+              />
+
+              <Button onPress={handleSubmit} title="Submit" />
+            </View>
+          )}
+        </Formik>
+    </View>
+  );
 };
 export default Payment;
