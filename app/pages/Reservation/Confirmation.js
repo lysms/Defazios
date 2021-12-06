36200b5c1d1baedcc6764e98b125f6851cc5028a
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-12-05 22:36:03 -0500

    reservation confirmation

--- app/pages/Reservation/Confirmation.js
+++ app/pages/Reservation/Confirmation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View, SafeAreaView} from 'react-native';
-import styles from './Confirmation.style'
+import styles from './Confirmation.style';
+import designs from '../../constants/designs';
 import * as Haptics from 'expo-haptics';
 import { Link } from 'react-router-native';
 import db from '../../firebase';
@@ -15,7 +16,7 @@ const Confirmation = (props) => {
   }
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.pageContent}>
       <View style={styles.viewForComfirmationTable}>
 
         <View style={styles.confirmTable}>
@@ -23,32 +24,32 @@ const Confirmation = (props) => {
             <Text style={{fontSize: 19, fontWeight: 'bold',}}>Confirmation Details</Text>
           </View>
 
-          <View style={{justifyContent: 'space-around', marginHorizontal: 3, marginVertical: 7,}}>
+          <View style={styles.confirmContainer}>
 
             <View style={styles.infoBoxForLine}>
-              <Text style={styles.infoText}>Name</Text>
+              <Text style={styles.infoText}>Name:</Text>
               <Text style={styles.infoText}>{details.firstName} {details.lastName}</Text>
             </View>
 
             <View style={styles.infoBoxForLine}>
-              <Text style={styles.infoText}>Phone Number</Text>
+              <Text style={styles.infoText}>Phone Number:</Text>
               <Text style={styles.infoText}>{details.phone}</Text>
             </View>
 
             <View style={styles.infoBoxForLine}>
-              <Text style={styles.infoText}>Email Address</Text>
+              <Text style={styles.infoText}>Email Address:</Text>
               <Text style={styles.infoText}>{details.email}</Text>
             </View>
 
             <View style={styles.infoBoxForLine}>
-              <Text style={styles.infoText}>Date</Text>
+              <Text style={styles.infoText}>Date:</Text>
               <Text style={styles.infoText}>
                 {details.date.getMonth() + 1}/{details.date.getDate()}/{details.date.getFullYear()}
               </Text>
             </View>
 
             <View style={styles.infoBoxForLine}>
-              <Text style={styles.infoText}>Time</Text>
+              <Text style={styles.infoText}>Time:</Text>
               <Text style={styles.infoText}>
                 {(24 - details.date.getHours()) + 1}:{details.date.getMinutes()}
               </Text>
@@ -62,12 +63,13 @@ const Confirmation = (props) => {
           </View>
           </View>
       </View>
+
       <View style={styles.viewForReturnBtn}>
-        <Link to={{pathname:"/"}} style={styles.l_returnBtn} onPress={()=>{handleReservation(); Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}>
-          <Text style={styles.l_textInReturnBtn}>Confirm Reservation</Text>
+        <Link to={{pathname:"/"}} onPress={()=>Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}>
+          <Text style={designs.optionText}>Cancel Reservation</Text>
         </Link>
-        <Link to={{pathname:"/"}} style={styles.l_returnBtn} onPress={()=>Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}>
-          <Text style={styles.l_textInReturnBtn}>Cancel Reservation</Text>
+        <Link to={{pathname:"/"}} onPress={()=>{handleReservation(); Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)}}>
+          <Text style={designs.optionText}>Confirm Reservation</Text>
         </Link>
       </View>
     </SafeAreaView>
