2973cac611552ad42a748306b4ce4b1355d4916e
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-12-05 22:00:03 -0500

    confirmation page

--- app/components/Confirmation/Confirmation.js
+++ app/components/Confirmation/Confirmation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View,TouchableOpacity } from 'react-native';
+import { Text, View,TouchableOpacity, ScrollView } from 'react-native';
 import styles from './Confirmation.style';
 import { Link } from 'react-router-native';
 
@@ -7,20 +7,32 @@ import { Link } from 'react-router-native';
 const Confirmation = props => {
 
   const order = props.finalOrder;
+  console.log(order)
+
+
+  let items = 
+    order.items.map(el => {
+      return (
+        <View key={el.id} style={styles.itemMain}>
+          <Text style={styles.itemTitle}>{el.name}</Text>
+          <View style={styles.itemNums}>
+            <Text>Quantity: {el.quantity}</Text>
+            <Text>Cost: {el.cost.toFixed(2)}</Text>
+          </View>
+        </View>
+      )
+    })
   
   return (
     <View style={styles.container}>
 
-      <View style={styles.container3}>
-      </View>
-
       <View style={styles.container1}>
         <View style={styles.details}>
-          <Text style={styles.regularText}>
+          <Text style={styles.boldText}>
             Confirmation Details
           </Text>
 
-          <Text style={styles.regularText}>{order.fname} {order.lname}</Text>
+          <Text style={styles.regularText}>Name: {order.fname} {order.lname}</Text>
 
           <View style={styles.innerText}>
 
@@ -36,37 +48,25 @@ const Confirmation = props => {
           <Text style={styles.regularText}>Phone Number: {order.phone}</Text>
           <Text style={styles.regularText}>Email: {order.email}</Text>
 
-          {
-            order.items.map(el => {
-
-              return (
-                <View key={el.id}>
-                  <View>
-                    <Text>{el.name}</Text>
-                    <Text>Quantity: {el.quantity}</Text>
-                  </View>
-                  <Text>${el.cost}</Text>
-                </View>
-              );
-            })
-          }
-
+          <ScrollView>
+            { items }
+          </ScrollView>
           <Text style={styles.regularText}>Subtotal: ${order.subTotal.toFixed(2)}</Text>
 
         </View>
 
       </View>
 
+
       <View style={styles.container2}>
+        
+        <Link to='/'>
+          <Text style={styles.optionText}>Cancel Order</Text>
+          </Link>
       
         <Link to='/' onPress={props.confirmOrder}>
-          <Text>
-            Confirm Order 
-          </Text>
+          <Text style={styles.optionText}>Confirm Order</Text>
         </Link>
-      </View>
-
-      <View style={styles.container3}>
       </View>
 
     </View>
--- app/components/Confirmation/Confirmation.style.js
+++ app/components/Confirmation/Confirmation.style.js
@@ -1,78 +1,102 @@
 import { StyleSheet, Dimensions } from 'react-native';
-
+import colors from '../../constants/colors';
 const screen = Dimensions.get('window')
 
 
 export default StyleSheet.create({
-    container: {
-        flex: 2,
-        backgroundColor: '#fff',
-    },
+  container: {
+    flex: 2,
+    backgroundColor: '#fff'
+  },
 
-    container1: {
-        flex: 20,
-        justifyContent: 'center',
-        alignItems: 'flex-start'
-    },
+  container1: {
+    flex: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
 
-    confirm: {
-        fontSize: 30,
-        marginLeft: screen.width / 80,
-        padding: 10,
-        fontWeight: 'bold',
-    },
+  confirm: {
+    fontSize: 30,
+    marginLeft: screen.width / 80,
+    padding: 10,
+    fontWeight: 'bold',
+  },
 
-    details: {
-        borderWidth: 2,
-        marginLeft: screen.width / 21,
-        padding: 10,
-        width: screen.width / 1.1,
-        borderColor: "grey"
-    },
+  details: {
+    borderWidth: 1,
+    marginLeft: screen.width / 21,
+    padding: 10,
+    width: screen.width / 1.1,
+    borderColor: "grey",
+    backgroundColor: colors.lightGrayText
+  },
 
-    innerText: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
+  innerText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 
-    regularText: {
-        padding: 5,
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
+  regularText: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
 
-    specialText: {
-        color: 'black',
-        padding: 5,
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
+  specialText: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
 
-    createAccount: {
-        width: '50%',
-        height: 65,
-        color: "white",
-        backgroundColor: "black",
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 15
-    },
+  createAccount: {
+    width: '50%',
+    height: 65,
+    color: "white",
+    backgroundColor: "black",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15
+  },
 
-    textcreate: {
-        fontSize: 20,
-        color: "white",
-        fontWeight: "bold"
-    },
+  textcreate: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold"
+  },
 
-    container2: {
-        justifyContent: 'center',
-        alignItems: 'center',
+  container2: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 25
+  },
+  container3: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
 
-        padding: 20
-    },
-    container3: {
-        flex: 5,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
+  itemMain: {
+    flexDirection: 'row',
+    width: '95%',
+    justifyContent: 'space-between',
+    marginVertical: 3
+  },
+
+  itemTitle: {
+    fontWeight: 'bold'
+  },
+
+  itemNums: {
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+
+  boldText: {
+    fontWeight: 'bold', 
+    fontSize: 20
+  },
+
+  optionText: {
+    color: colors.blueText
+  },
 })
