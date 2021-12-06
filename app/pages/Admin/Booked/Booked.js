25ad9a42589a17569be97efce5117e11475fdacc
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-12-06 01:13:59 -0500

    adding functionality to view reservations

--- app/pages/Admin/Booked/Booked.js
+++ app/pages/Admin/Booked/Booked.js
@@ -6,86 +6,115 @@ import styles from './Booked.style'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { MaterialIcons, AntDesign } from '@expo/vector-icons';
+import db from '../../../firebase';
 
 const Tap = createBottomTabNavigator();
 
 const Booked = ({ history }) => {
-    return (
-        <View style={styles.container}>
+
+  const [orderItems, setOrderItems] = useState([]);
+  
+  let orders = []
+  useEffect(() => {
+    
+    db.collection('reservations').get()
+      .then(snap => {
+        
+        snap.forEach(res => {
+          let data = res.data();
+          let newObj = {
+            fname: data.firstName,
+            lname: data.lastName,
+            dateFor: data.date.toDate().toString(),
+            datePlaced: data.datePlaced.toDate().toString(),
+            email: data.email,
+            phone: data.phone,
+            size: data.size
+          }
+
+          orders.push(newObj)
+          console.log(orders)
+
+        })
+
+        setOrderItems([...orders])
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
 
 
-            <View style={styles.container1}>
-                <Text style={styles.headerText}>View the Upcoming Reservation. </Text>
-            </View>
+  return (
+    <View style={styles.container}>
 
 
-            <View style={styles.profile}>
-                <ScrollView style={styles.orderHistory}>
-                    <Text style={styles.text}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                    </Text>
-                    <Text style={styles.text}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                    </Text>
-                    <Text style={styles.text}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
-                    </Text>
-                </ScrollView>
-            </View>
+      <View style={styles.container1}>
+          <Text style={styles.headerText}>View the Upcoming Reservation. </Text>
+      </View>
 
 
-            <View style={styles.container3}></View>
-            <View style={styles.container3}></View>
-            {/* Footer component */}
+      <View style={styles.profile}>
+        <ScrollView style={styles.orderHistory}>
+        {
+          orderItems.length > 0 ?
 
-            <View style={styles.menucontainer}>
-                {/* <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/')}>
-                    <Text style={styles.textcreate}>Home</Text>
-                </TouchableOpacity> */}
+            orderItems.map((data, i) => {
 
-                <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminMenus')}>
-                    <Text style={styles.textcreate}>Menus</Text>
-                </TouchableOpacity>
+              return (
+                <View key={i}>
+                  <Text></Text>
+                  <TouchableOpacity onPress={() => handleDetails(data)}>
+                    <View style={styles.minimenucontainer2}>
+                      <Text key={i - 1} style={styles.subitem}>{data.fname}, {data.lname}</Text>
 
-                <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminOrder')}>
-                    <Text style={styles.textcreate}>Orders</Text>
-                </TouchableOpacity>
+                      <Text key={i + 1} style={styles.subitem}>{data.dateFor.substring(0, 10)} {data.dateFor.substring(15, 21)} {data.dateFor.substring(34, 39)}</Text>
 
-                <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminHome')}>
-                    <Text style={styles.textcreate}>Profile</Text>
-                </TouchableOpacity>
+                    </View>
+                  </TouchableOpacity>
 
-                <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminReservation')}>
-                    <Text style={styles.textcreate1}>Booked</Text>
-                </TouchableOpacity>
+                </View>
+              )
+            }) : <View></View>
+        }
+        </ScrollView>
+      </View>
 
-                {/* <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminNews')}>
-                    <Text style={styles.textcreate}>News</Text>
-                </TouchableOpacity> */}
-            </View>
 
+        <View style={styles.container3}></View>
+        <View style={styles.container3}></View>
+        {/* Footer component */}
+
+        <View style={styles.menucontainer}>
+            {/* <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/')}>
+                <Text style={styles.textcreate}>Home</Text>
+            </TouchableOpacity> */}
+
+            <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminMenus')}>
+                <Text style={styles.textcreate}>Menus</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminOrder')}>
+                <Text style={styles.textcreate}>Orders</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminHome')}>
+                <Text style={styles.textcreate}>Profile</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminReservation')}>
+                <Text style={styles.textcreate1}>Booked</Text>
+            </TouchableOpacity>
+
+            {/* <TouchableOpacity style={styles.createAccount} onPress={() => history.push('/adminNews')}>
+                <Text style={styles.textcreate}>News</Text>
+            </TouchableOpacity> */}
         </View>
 
+    </View>
 
-    );
+
+  );
 };
 
 export default Booked;
