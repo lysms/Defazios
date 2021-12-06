81f3f5b57dfae5b4c20a20a1a77ffd6df0d5eef6
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-12-06 00:06:20 -0500

    admin home

--- app/pages/Admin/Admin_Home/Admin_Home.style.js
+++ app/pages/Admin/Admin_Home/Admin_Home.style.js
@@ -5,111 +5,116 @@ const screen = Dimensions.get('window')
 
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    height: '100%'
 
-    },
+  },
 
-    container1: {
-        flex: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
+  container1: {
+    flex: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
 
-    headerText: {
-        fontSize: 30,
-        padding: 10,
-        textAlign: 'center',
-        fontWeight: 'bold',
-    },
+  headerText: {
+    fontSize: 30,
+    padding: 10,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
 
-    headerText2: {
-        fontSize: 20,
-        padding: 10,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        color: "orange"
-    },
+  headerText2: {
+    fontSize: 20,
+    padding: 10,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: "black"
+  },
 
-    profile: {
-        flex: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-    },
+  profile: {
+    flex: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
 
-    text: {
-        textAlign: 'center',
-    },
+  text: {
+    textAlign: 'center',
+  },
 
-    menucontainer: {
-        flex: 8,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        backgroundColor: COLORS.lightGray,
-        width: screen.width,
-        height: 65,
-    },
-
-
-    createAccount: {
-
-        color: "black",
-        justifyContent: 'center',
-        alignItems: 'center',
-
-    },
+  menucontainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: COLORS.lightGray,
+    width: screen.width,
+    height: 65,
+  },
 
 
-    textcreate: {
-        fontSize: 18,
-        // width: screen.width / 4,
-        textAlign: 'center',
-        color: "black",
+  createAccount: {
 
-    },
+    color: "black",
+    justifyContent: 'center',
+    alignItems: 'center',
 
-    textcreate1: {
-        fontSize: 18,
-        textAlign: 'center',
-        color: "black",
-        fontWeight: "bold",
-    },
+  },
 
-    container2: {
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    container3: {
-        flex: 5,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
 
-    container4: {
-        justifyContent: 'center',
-        alignItems: 'flex-end',
-        padding: 20
-    },
+  textcreate: {
+    fontSize: 18,
+    // width: screen.width / 4,
+    textAlign: 'center',
+    color: "black",
 
-    buttonContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    button: {
-        backgroundColor: '#0782F9',
-        width: screen.width / 3.8,
-        padding: 15,
-        borderRadius: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
+  },
 
-    },
+  textcreate1: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: "black",
+    fontWeight: "bold",
+  },
 
-    buttonText: {
-        color: "white",
-        fontWeight: '700',
-        fontSize: 18,
-    }
+  container2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container3: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  container4: {
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    padding: 20
+  },
+
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: 'black',
+    width: screen.width / 3.8,
+    padding: 15,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+
+  buttonText: {
+    color: "white",
+    fontWeight: '700',
+    fontSize: 18,
+  },
+
+  contentContainer: {
+    height: '92%',
+    justifyContent: 'space-between'
+  }
 })
