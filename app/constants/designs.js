96ffbd106bd48e207c920a261717afc5510dee30
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-12-05 22:21:50 -0500

    reservation info page

--- app/constants/designs.js
+++ app/constants/designs.js
@@ -1,5 +1,71 @@
 import {StyleSheet } from 'react-native';
+import COLORS from './colors';
 
+export default StyleSheet.create({
+  optionText: {
+    color: COLORS.blueText
+  },
 
-export default DESIGNS = {
-}
+  linkContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  infoText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+
+  infoInput: {
+    fontSize: 17,
+    padding: 7,
+    borderRadius: 6,
+    backgroundColor: COLORS.lightGrayText,
+    height: 34,
+    borderColor: COLORS.lightGray,
+    borderWidth: 1,
+    margin: 5,
+    width: '97%',
+  },
+
+  chooseContainer: {
+    paddingTop: 40,
+    paddingBottom: 30,
+    width: '100%'
+  },
+
+  infoContainer: {
+    flex: 1,
+    paddingTop: 40,
+  },
+
+  chooseItem: {
+    marginVertical: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+
+  chooseBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 12,
+    borderRadius: 6,
+    backgroundColor: "black",
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+    shadowOffset: {
+        height: 2,
+        width: 1
+    },
+    elevation: 2,
+    marginBottom: 2, 
+    width: '100%'
+  },
+
+  chooseBtnText: {
+    color: 'white',
+  },
+});
--- app/pages/Reservation/Information.js
+++ app/pages/Reservation/Information.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import {  Text, View, SafeAreaView, TextInput, Button, TouchableOpacity } from 'react-native';
-import styles from './Information.style'
+
+import styles from './Information.style';
+import designs from '../../constants/designs';
+
 import * as Haptics from 'expo-haptics';
 import { Link } from 'react-router-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -49,12 +52,7 @@ const Information = props => {
 
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={styles.header}>
-        <Link to={{pathname:"/reservation"}} style={styles.returnBtn} onPress={()=>Haptics.ImpactFeedbackStyle.Light}>
-          <Text style={styles.textInsideReturnBtn}> Return </Text>
-        </Link>
-      </View>
+    <SafeAreaView style={designs.infoContainer}>
       <View style={styles.viewForInput}>
 
         <Text style={styles.textForTitle}>Enter Confirmation Details</Text>
@@ -70,81 +68,103 @@ const Information = props => {
         >
 
           {({handleChange, handleBlur, handleSubmit, values}) => (
-
-            <View>
-
-              <Text style={styles.infoText}>First Name</Text>
-              <TextInput 
-                style={styles.infoInput}
-                placeholder="First Name"
-                onChangeText={handleChange('firstName')}
-                onBlur={handleBlur('firstName')}
-                value={values.firstName}
-              />
-
-              <Text style={styles.infoText}>Last Name</Text>
-              <TextInput 
-                style={styles.infoInput}
-                placeholder="Last Name"
-                onChangeText={handleChange('lastName')}
-                onBlur={handleBlur('lastName')}
-                value={values.lastName}
-              />
-                  
-              <Text style={styles.infoText}>Phone Number</Text>
-              <TextInput 
-                style={styles.infoInput}
-                placeholder="###-###-####"
-                onChangeText={handleChange('phone')}
-                onBlur={handleBlur('phone')}
-                value={values.phone}
-              />
-
-              <Text style={styles.infoText}>Email Address</Text>
-              <TextInput
-                style={styles.infoInput}
-                placeholder="johndoe@email.com"
-                onChangeText={handleChange('email')}
-                onBlur={handleBlur('email')}
-                value={values.email}
-              />
-
-              <Text style={styles.infoText}>Party Size</Text>
-              <TextInput
-                style={styles.infoInput}
-                placeholder="2-10"
-                onChangeText={handleChange('size')}
-                onBlur={handleBlur('size')}
-                value={values.size}
-              />
+            
+            
+            <View style={styles.infoContent}>
 
               <View>
-
-              <View>
-                  <Button onPress={showDatepicker} title="Show date picker!" />
-              </View>
-              <Text>Selected date: {date.getMonth() + 1}/{date.getDate()}</Text>
-
-              <View>
-                <Button onPress={showTimepicker} title="Show time picker!" />
-              </View>
-
-              <Text>Selected time: {hours}:{minutes}</Text>
-              {show && (
-                <DateTimePicker
-                  testID="dateTimePicker"
-                  value={date}
-                  mode={mode}
-                  is24Hour={true}
-                  display="default"
-                  onChange={onChange}
+                <Text style={designs.infoText}>First Name</Text>
+                <TextInput 
+                  style={designs.infoInput}
+                  placeholder="First Name"
+                  onChangeText={handleChange('firstName')}
+                  onBlur={handleBlur('firstName')}
+                  value={values.firstName}
                 />
-              )}
-              </View> 
 
-              <TouchableOpacity onPress={handleSubmit}>
-                <Text>Confirm Reservation</Text>
-              </TouchableOpacity>
+                <Text style={designs.infoText}>Last Name</Text>
+                <TextInput 
+                  style={designs.infoInput}
+                  placeholder="Last Name"
+                  onChangeText={handleChange('lastName')}
+                  onBlur={handleBlur('lastName')}
+                  value={values.lastName}
+                />
+                    
+                <Text style={designs.infoText}>Phone Number</Text>
+                <TextInput 
+                  style={designs.infoInput}
+                  placeholder="###-###-####"
+                  onChangeText={handleChange('phone')}
+                  onBlur={handleBlur('phone')}
+                  value={values.phone}
+                />
+
+                <Text style={designs.infoText}>Email Address</Text>
+                <TextInput
+                  style={designs.infoInput}
+                  placeholder="johndoe@email.com"
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                />
+
+                <Text style={designs.infoText}>Party Size</Text>
+                <TextInput
+                  style={designs.infoInput}
+                  placeholder="2-10"
+                  onChangeText={handleChange('size')}
+                  onBlur={handleBlur('size')}
+                  value={values.size}
+                />
+
+                <View style={designs.chooseContainer}>
+
+                  <View style={designs.chooseItem}>
+                    <TouchableOpacity onPress={showDatepicker} style={designs.chooseBtn}>
+                      <Text style={designs.chooseBtnText}>Choose Date</Text>
+                    </TouchableOpacity>
+
+                    <Text>
+                      Selected Date: {(date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear()}
+                    </Text>
+                  </View>
+
+                  <View style={designs.chooseItem}>
+                    <TouchableOpacity onPress={showTimepicker} style={designs.chooseBtn}>
+                      <Text style={designs.chooseBtnText}>Choose Time</Text>
+                    </TouchableOpacity>
+
+                  <Text>
+                    Time: {date.getHours() + ":" + date.getMinutes()}
+                  </Text>
+                </View>
+
+                  {show && (
+                    <DateTimePicker
+                      testID="dateTimePicker"
+                      value={date}
+                      mode={mode}
+                      is24Hour={true}
+                      display="default"
+                      onChange={onChange}
+                    />
+                  )}
+                </View> 
+
+              </View>
+
+
+              <View style={designs.linkContainer}>
+                <Link to={{pathname:"/reservation"}} onPress={()=>Haptics.ImpactFeedbackStyle.Light}>
+                  <Text style={designs.optionText}> Return </Text>
+                </Link>
+
+                <TouchableOpacity onPress={handleSubmit}>
+                  <Text style={designs.optionText}>Confirm Reservation</Text>
+                </TouchableOpacity>
+              </View>
+
 
             </View>
           )}
